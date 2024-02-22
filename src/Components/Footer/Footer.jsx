3d6c5162599ac967dd52bf782acb3a4e559a6a49
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-02-22 15:01:25 +0530

    Implement Footer Component - Create a 'Footer' component for displaying website footer - Utilize React Router's 'Link' component for navigation - Integrate a logo using an image from the 'assets' folder - Include links to 'About' and 'Contact-Us' pages for enhanced navigation This commit introduces the 'Footer' component, responsible for rendering the website footer. It includes a logo sourced from the 'assets' folder and uses React Router's 'Link' component to provide seamless navigation to the 'About' and 'Contact-Us' pages. The component enhances the overall user experience by providing easy access to key sections of the website.

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
+
+export const Footer = () => {
+    return (
+        <div className="bg-black flex flex-row justify-around p-4 ">
+            <img src={logo} ></img>
+            <div className="text-white font-inter text-3md flex flex-col justify-center">
+                <Link to="/about" >About</Link>
+                <Link to="/contact-us">Contact-Us</Link>
+            </div>
+        </div>
+    )
+}
