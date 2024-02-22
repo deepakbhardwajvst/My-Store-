32fbdc47360fde114fa1b43882c02288580bc233
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-02-22 14:59:35 +0530

    Implement FilteredProducts Component - Utilize React Router for dynamic route parameters - Fetch and display filtered products based on the 'type' parameter - Implement 'useEffect' to scroll to the top on page load - Enhance UI with NavigationButtons for improved navigation - Optimize grid layout for responsiveness across various screen sizes This commit introduces the 'FilteredProducts' component, leveraging React Router to handle dynamic route parameters. The component fetches and displays products filtered by the 'type' parameter. Additionally, it utilizes 'useEffect' to scroll to the top on page load and includes 'NavigationButtons' for a seamless navigation experience. The grid layout is optimized for responsiveness on different screen sizes.

---
+++ src/Components/FilteredProducts/FilteredProducts.jsx
@@ -0,0 +1,45 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useParams } from "react-router-dom";
+import { ProductCard } from "../ProductCard/ProductCard";
+import { useEffect } from "react";
+import { NavigationButtons } from "../NavigationButtons/NavigationButtons";
+
+export const FilteredProducts = () => {
+    const dispatch = useDispatch();
+    const { type } = useParams();
+    const products = useSelector((state) => state.products.filteredProducts);
+    useEffect(() => {
+        // scroll to top on page load
+        window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    }, []);
+    return (
+        <div className="bg-pink-50 ">
+            <div className="p-10">
+                <div className="py-8 mb-2 flex justify-center ">
+                    <h1 className="text-4xl font-inter text-gray-600 font-bold tracking-normal leading-none">
+                        {type}
+                    </h1>
+                </div>
+                <NavigationButtons />
+                <div className="grid grid-cols-1 justify-items-center py-8 gap-12 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
+                    {products
+                        .filter((product) => product.type === type)
+                        .map((product, index) => {
+                            return (
+                                <ProductCard
+                                    key={index}
+                                    id={product.id}
+                                    name={product.name}
+                                    img={product.img}
+                                    price={product.price}
+                                    text={product.text}
+                                    colors={product.color}
+                                    type={product.type}
+                                />
+                            );
+                        })}
+                </div>
+            </div>
+        </div>
+    );
+};
