b43b1c5d3259cc05912a327736bea50ffd151d1e
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-02-22 15:07:16 +0530

    Implement NavigationButtons Component - Introduce 'NavigationButtons' component for dynamic product filtering - Utilize Material Tailwind's 'Button' component for UI consistency - Integrate React Router's 'Link' for seamless navigation to filtered products - Implement Redux 'filterProducts' action for state management - Enhance visual appeal with responsive styling and hover effects This commit adds the 'NavigationButtons' component, providing a dynamic way to filter products based on different categories. The component utilizes Material Tailwind's 'Button' component for consistent UI elements and React Router's 'Link' for smooth navigation to filtered products. The integration of the Redux 'filterProducts' action ensures effective state management. Visual enhancements include responsive styling and hover effects for a polished user experience.

---
+++ src/Components/NavigationButtons/NavigationButtons.jsx
@@ -0,0 +1,43 @@
+import { Button } from "@material-tailwind/react";
+import { useDispatch } from "react-redux";
+import { filterProducts } from "../../features/slices/productSlice";
+import { Link } from "react-router-dom";
+
+export const NavigationButtons = () => {
+    const buttons = [
+        "Hoodies",
+        "Dresses",
+        "Suits",
+        "Shoes",
+        "T-Shirts",
+        "Jeans",
+        "Jackets",
+        "Bags",
+    ];
+
+    const dispatch = useDispatch();
+
+    return (
+
+        <div className="flex item-center justify-center py-8 sm:max-w-full flex-wrap gap-2">
+            {buttons.map((button, index) => {
+                return (
+                    <div key={index} className="mr-4">
+                        <Link to={"/filteredProducts/" + button}>
+                            <Button
+                                color="gray"
+                                size="lg"
+                                variant="outlined"
+                                ripple={true}
+                                className="hove:bg-green-300 duration-300 ease-in-out sm:w-200"
+                                onClick={() => dispatch(filterProducts(button))}
+                            >
+                                {button}
+                            </Button>
+                        </Link>
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
