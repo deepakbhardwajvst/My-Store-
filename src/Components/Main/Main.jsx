272630cfcc8069998ca9370fffe3b2498ca43192
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-02-22 14:56:23 +0530

    Refactor Main component structure - Reorganize the Main component for improved readability and maintainability - Import and include necessary components in a logical order This commit focuses on restructuring the Main component by incorporating components in a more organized manner. The changes aim to enhance code clarity and maintainability, promoting a cleaner component structure.

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import { Slider } from "../Slider/Slider";
+import { Header } from "../Header/Header";
+import { NavigationButtons } from "../NavigationButtons/NavigationButtons";
+import { Footer } from "../Footer/Footer";
+import { ProductBar } from "../ProductBar/ProductBar";
+
+export const Main = () => {
+    return (
+        <div>
+            <Slider />
+            <NavigationButtons />
+            <ProductBar />
+        </div>
+    );
+};
