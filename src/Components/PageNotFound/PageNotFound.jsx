019bbda3152bab9a7db1bdd8bc84b42cb15900aa
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-02-27 14:50:24 +0530

    Updated PageNotFound component: - Added a background color to improve visibility (bg-pink-100). - Centered the content vertically and horizontally (flex justify-center items-center h-[100vh]). - Used the 'font-inter' font family for the text (text-4xl font-bold font-inter). - Improved commit messages for better clarity.

---
+++ src/Components/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,7 @@
+export const PageNotFound = ()=>{
+    return (
+        <div className="bg-pink-100 flex justify-center items-center h-[100vh]">
+            <h1 className="text-4xl font-bold font-inter">Page Not Found</h1>
+        </div>
+    )
+}
